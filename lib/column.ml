0194878c8755095e1134d3ab4cbd112f8191b4c1
Anthony Song <anthony.b.song@gmail.com>
2024-03-26 19:50:41 -0400

    - Finished but have not tested the functionality of datatable (dt) and columns - Wrote a Make function that can make a column and/or make a dt given a file input (being tested)

--- lib/column.ml
+++ lib/column.ml
@@ -1,7 +1,43 @@
-type cell =
-  | Int of int
-  | String of string
-  | Float of float
-  | Boolean of int
+exception Not_found
+exception Out_of_bounds
 
-type t = cell list
+type option =
+  | Some of float
+  | None
+
+type t = {
+  mutable data : option array;
+  mutable size : int;
+}
+
+let empty capacity = { data = Array.make capacity None; size = 0 }
+let size col = col.size
+
+let add col elt =
+  let () = col.data.(col.size) <- elt in
+  col.size <- col.size + 1
+
+let get col idx =
+  if idx >= size col then raise Out_of_bounds
+  else
+    match col.data.(idx) with
+    | None -> raise Not_found
+    | Some x -> x
+
+let rec make lst col =
+  if List.length lst <> size col then raise Out_of_bounds
+  else
+    match lst with
+    | [] -> col
+    | h :: t ->
+        let () = add col (Some h) in
+        make t col
+
+let rec make_string lst col =
+  if List.length lst <> size col then raise Out_of_bounds
+  else
+    match lst with
+    | [] -> col
+    | h :: t ->
+        let () = add col (Some (float_of_string h)) in
+        make_string t col
--- lib/dt.ml
+++ lib/dt.ml
@@ -1,20 +1,34 @@
-open Column
-
 exception EmptyTable
 
-type t = {
-  mutable headers : string list;
-  mutable dt : Column.t list;
+type 'a t = {
+  mutable headers : string array;
+  mutable dt : Column.t array;
+  mutable size : int;
 }
 
-let empty = []
+let empty col row =
+  {
+    headers = Array.make col " ";
+    dt = Array.make col (Column.empty row);
+    size = 0;
+  }
 
-let is_empty table =
-  match table.dt with
-  | [] -> true
-  | _ -> false
+let add table name (col : Column.t) =
+  let () = table.headers.(col.size) <- name in
+  let () = table.dt.(col.size) <- col in
+  col.size <- col.size + 1
 
-let get_rows table =
-  match table.dt with
-  | [] -> 0
-  | h :: _ -> List.length h
+let rec make_helper table (row : int) names = function
+  | [] -> ()
+  | h :: t -> begin
+      match names with
+      | [] -> ()
+      | name :: t2 ->
+          let () = add table name (Column.make_string h (Column.empty row)) in
+          make_helper table row t2 t
+    end
+
+let make file names dt =
+  let columns = Csv.transpose (Csv.load file) in
+  let rows = List.length (List.nth columns 0) in
+  make_helper dt rows names columns
