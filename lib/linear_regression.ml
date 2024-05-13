78ce9b826667bd7f30849c72e9cb54cff632015e
eric-zzhou <ericsuper@outlook.com>
2024-05-13 19:47:53 -0400

    Got rid of Owl dependency, using torch instead

--- lib/linear_regression.ml
+++ lib/linear_regression.ml
@@ -1,37 +1,32 @@
-open Owl
+open Torch
 
 let theta x y =
   (* Convert input data to matrix and vector *)
-  let x_mat = Mat.transpose (Mat.of_arrays x) in
-  let y_vec = Mat.transpose (Mat.of_array y 1 (Array.length y)) in
+  let y = [| y |] in
+  let x_mat = Tensor.transpose (Tensor.of_float2 x) ~dim0:0 ~dim1:1 in
+  let y_vec = Tensor.transpose (Tensor.of_float2 y) ~dim0:0 ~dim1:1 in
 
   (* Print the shapes of x_mat and y_vec *)
-  Printf.printf "Shape of x_mat: (%d, %d)\n"
-    (fst (Mat.shape x_mat))
-    (snd (Mat.shape x_mat));
-  Printf.printf "Shape of y_vec: (%d, %d)\n"
-    (fst (Mat.shape y_vec))
-    (snd (Mat.shape y_vec));
+  let x_shape = Array.of_list (Tensor.shape x_mat) in
+  let y_shape = Array.of_list (Tensor.shape y_vec) in
+  Printf.printf "Shape of x_mat: (%d, %d)\n" x_shape.(0) x_shape.(1);
+  Printf.printf "Shape of y_vec: (%d, %d)\n" y_shape.(0) y_shape.(1);
 
   (* Matrix computations *)
-  let xt = Mat.transpose x_mat in
+  let xt = Tensor.transpose x_mat ~dim0:0 ~dim1:1 in
   (* Print the shapes of all intermediate matrices *)
-  Printf.printf "Shape of xt: (%d, %d)\n"
-    (fst (Mat.shape xt))
-    (snd (Mat.shape xt));
-  let xtx = Mat.dot xt x_mat in
-  Printf.printf "Shape of xtx: (%d, %d)\n"
-    (fst (Mat.shape xtx))
-    (snd (Mat.shape xtx));
-  let xtx_inv = Mat.inv xtx in
-  Printf.printf "Shape of xtx_inv: (%d, %d)\n"
-    (fst (Mat.shape xtx_inv))
-    (snd (Mat.shape xtx_inv));
-  let xty = Mat.dot xt y_vec in
-
-  Printf.printf "Shape of xty: (%d, %d)\n"
-    (fst (Mat.shape xty))
-    (snd (Mat.shape xty));
+  let xt_shape = Array.of_list (Tensor.shape xt) in
+  Printf.printf "Shape of xt: (%d, %d)\n" xt_shape.(0) xt_shape.(1);
+  let xtx = Tensor.matmul xt x_mat in
+  let xtx_shape = Array.of_list (Tensor.shape xtx) in
+  Printf.printf "Shape of xtx: (%d, %d)\n" xtx_shape.(0) xtx_shape.(1);
+  let xtx_inv = Tensor.inverse xtx in
+  let xtx_inv_shape = Array.of_list (Tensor.shape xtx_inv) in
+  Printf.printf "Shape of xtx_inv: (%d, %d)\n" xtx_inv_shape.(0)
+    xtx_inv_shape.(1);
+  let xty = Tensor.matmul xt y_vec in
+  let xty_shape = Array.of_list (Tensor.shape xty) in
+  Printf.printf "Shape of xty: (%d, %d)\n" xty_shape.(0) xty_shape.(1);
 
   (* Final matrix multiplication to compute regression coefficients *)
-  Mat.dot xtx_inv xty
+  Tensor.matmul xtx_inv xty
